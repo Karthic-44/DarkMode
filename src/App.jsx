e0fd3bdcae7ee5b4d3e8ade8debb213f9b749b08
Karthic <karthics004@gmail.com>
2025-09-10 19:07:54 +0530

    Final application

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
+import DarkModeToggle from './DarkModeToggle';
 
 function App() {
 
 
   return (
-  <div></div>
-  );
+  <div className='p-4 text-black bg-white min-h-screen dark:bg-gray-900 dark:text-white'>
+    <DarkModeToggle />
+    <h1 className='text 2x1'>Welcome To Dark Mode</h1>
+    <p>A simple darkmode using tailwind</p>
+
+  </div>
+  )
 }
 
 export default App
